6b79f032a354e6b218c434fa7133a33049a6b689
Michael Z <michael_zamora@comcast.net>
2022-02-13 03:17:20 -0700

    fix assertion string bugs

--- git-push.sh
+++ git-push.sh
@@ -22,7 +22,8 @@ success() {
 }
 
 assert() {
-    if [ ! "$1" ]
+    echo $1
+    if ! [ $1 ]
     then
         error "$2"
         until [ -z "$3" ]  # Until all parameters used up . . .
@@ -34,12 +35,17 @@ assert() {
     fi
 }
 
-assert "! $# -lt 2" "must supply path to local repository and commit message" \
+
+assert "$# -ge 2" "must supply path to local repository and commit message" \
 'Usage: ./git-push.sh <path-to-local-repo> "message"'
 
-assert '! -d $1/.git' "path does not point to a local git repository"
+assert "-d $1/.git" "path does not point to a local git repository"
 
-assert "-z '$2'" "commit message must not be empty"
+if [ -z "$2" ]
+then 
+    error "commit message must not be empty"
+    exit 1
+fi
 
 ORIGIN=$( git config --get remote.origin.url )
 echo "$( purple 'current remote origin' ): '$ORIGIN'"
@@ -51,8 +57,12 @@ then
 fi
 
 git pull origin main
-assert "$? -eq 0" "conflicting merge in main branch and local repository" "Solution: use 'git mergetool' to find and correct all merge conflicts." "Solution: if you want to overwrite local conflicts with remote state use 'git pull --force origin main'" "Solution: if you want to overwrite remote conflicts with local state use 'git push --force origin main'"
-success "pulled and merged main branch"
+assert "$? -eq 0" "could be from conflicting merge between main branch and local repository" \
+"Solution: use 'git mergetool' or 'git diff' to find and correct all merge conflicts." \
+"Solution: if you want to overwrite local conflicts with remote state use 'git pull --force origin main'" \
+"Solution: if you want to overwrite remote conflicts with local state use 'git push --force origin main'" \
+"Solution: Google how to resolve git merge conflicts"
+success "fetched and merged main branch"
 
 git add $1 && success "added '$1'" && \
 git commit -m "$2" && success "commited '$2'" && \
