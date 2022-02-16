92e8607570e961c30f68a6b7d43659b30dea55aa
Michael Z <michael_zamora@comcast.net>
2022-02-16 05:24:42 -0700

    add walk through first time setup

--- git-push.sh
+++ git-push.sh
@@ -1,33 +1,38 @@
 #!/usr/bin/env bash
 
-red() {
-    echo -e "\e[1;31m$1\e[0m"
-}
-green() {
-    echo -e "\e[1;32m$1\e[0m"
-}
-yellow() {
-    echo -e "\e[1;33m$1\e[0m"
-}
-purple() {
-    echo -e "\e[1;35m$1\e[0m"
-}
+# This script will push specified local changes to GitHub.
+# It takes two parameters:
+#   1: path to the file or directory you want to 'git add'
+#   2: commit message (ie. "fixed some bug")
+# You must execute this script from a local git repository
+# Complete Usage: 
+# 
+#   ./git-push <path-in-local-repo> "COMMIT MESSAGE"
+# 
 
+# First time setup can also be done using:
+# 
+#   curl https://raw.githubusercontent.com/mzamora1/CSE-360-Team-Project/main/git-push.sh | bash 
+# 
+# This will not push anything to GitHub. It will only configure git so that you can 
+# then push to GitHub manually or by rerunning this script with the correct arguments.
+
+red() { echo -e "\e[1;31m$1\e[0m"; }
+green() { echo -e "\e[1;32m$1\e[0m"; }
+yellow() { echo -e "\e[1;33m$1\e[0m"; }
+purple() { echo -e "\e[1;35m$1\e[0m"; }
+warning() { echo "$( yellow "WARNING" ): $1"; }
+success() { echo "$( green "SUCCESS" ): $1"; }
 error() {
     echo "$( red "ERROR" ): $1"
-}
-
-success() {
-    echo "$( green "SUCCESS" ): $1" 
+    return 1
 }
 
 assert() {
     # echo $1
-    if ! [ $1 ]
-    then
+    if ! [ $1 ]; then
         error "$2"
-        until [ -z "$3" ]  # Until all parameters used up . . .
-        do
+        until [ -z "$3" ]; do  # Until all parameters used up . . .
             echo "$3"
             shift
         done
@@ -35,41 +40,57 @@ assert() {
     fi
 }
 
+USAGE="$( purple "Usage" ): ./git-push.sh <path-in-local-repo> 'message'"
+CORRECT_ORIGIN="https://github.com/mzamora1/CSE-360-Team-Project.git"
 CWD=$( pwd )
-assert "-d $CWD/.git" "current working directory does not point to a local git repository" \
-"Solution: use 'cd <path-to-local-repo>'"
 
+if ! [ -d "$CWD/.git" ]; then
+    warning "current working directory does not point to a local git repository"
+    read -p "Do you want to clone the repository into its own folder in this directory? (yes/no)" CHOICE
+    case $CHOICE in
+        yes | y | Y | YES | 1) 
+            git clone $CORRECT_ORIGIN
+            success "cloned CSE-360-Team-Project to '$CWD/CSE-360-Team-Project'"
+            cd CSE-360-Team-Project
+            ;;
+        *)
+            error "rerun in a local git repository"
+            exit 1
+            ;;
+    esac
+fi
+
+ORIGIN=$( git config --get remote.origin.url )
 NAME=$( git config --get user.name )
 EMAIL=$( git config --get user.email )
-if [ "${NAME}d" == "d" ]
-then
-    read -p "Enter your first and last name. ex. John Smith (used in commit msg): " REALNAME
-    git config --global user.name $REALNAME
-fi
-if [ "${EMAIL}d" == "d" ]
-then
-    read -p "Enter your GitHub email (used in commit msg): " REALEMAIL
-    git config --global user.email $REALEMAIL
-fi
 
-
-assert "$# -ge 2" "must supply path to pass to git add and a commit message" \
-'Usage: ./git-push.sh <path-to-local-repo> "message"'
-
-if [ -z "$2" ]
-then 
-    error "commit message must not be empty"
-    exit 1
-fi
-
-CORRECT_ORIGIN="https://github.com/mzamora1/CSE-360-Team-Project.git"
-ORIGIN=$( git config --get remote.origin.url )
-echo "$( purple 'current remote origin' ): '$ORIGIN'"
-
-if [ $ORIGIN != $CORRECT_ORIGIN ]
-then
+if [ -z "$ORIGIN" ]; then
     echo "$( yellow 'adding remote origin' ): $CORRECT_ORIGIN"
     git remote add origin $CORRECT_ORIGIN
+elif [ $ORIGIN != $CORRECT_ORIGIN ]; then
+    echo "$( yellow 'setting remote origin' ): $CORRECT_ORIGIN"
+    git remote set-url origin $CORRECT_ORIGIN
+fi
+
+if [ -z "$NAME" ]; then
+    read -p "Enter your first and last name. ex. 'John Smith' (used in commit msg): " NAME
+    git config --global user.name $NAME
+fi
+
+if [ -z "$EMAIL" ]; then
+    read -p "Enter your GitHub email (used in commit msg): " EMAIL
+    git config --global user.email $EMAIL
+    success "use 'cd '$( pwd )'' and rerun this file to push changes to GitHub"
+fi
+
+
+# end of setup
+
+assert "$# -ge 2" "must supply path to pass to git add and a commit message" "$USAGE"
+
+if [ -z "$2" ]; then 
+    error "commit message must not be empty"
+    exit 1
 fi
 
 
